df7ab00708c68fb0cfa945b32825e30b9f207a63
Harry Kodden <harry@e-tunity.nl>
2022-01-06 11:18:49 +0100

    proper handling of create/delete

--- mount/renew.sh
+++ mount/renew.sh
@@ -3,6 +3,7 @@
 RENEW=false
 TOUCH=/etc/renewed
 
+# In order to minimize overhead, we only renew if there are actual updates
 if [ -f "$TOUCH" ]; then
     for f in /etc/mounts/*.conf
     do
@@ -15,18 +16,27 @@ else
     RENEW=true
 fi
 
+# Remove broken links to no longer existing pam files...
+find /etc/pam.d -xtype l -delete
+
+# No updates, get out !
 if [ $RENEW = false ]; then 
     exit 0
 fi
 
 touch "$TOUCH"
 
+# Find master nginx process...
 NGINX=$(ps -ax|grep nginx|grep master|awk '{print $1;}')
 
+# Reload configurations...
 kill -HUP $NGINX 
 
-for f in /etc/mounts/*.conf
+# Activate corresponding pam configurationsx
+for f in /etc/mounts/*.pam
 do 
-    p=$(basename "$f" .conf)
-    ln -sf /etc/mounts/$p /etc/pam.d/$p
+    p=$(basename "$f" .pam)
+    ln -sf $f /etc/pam.d/$p
 done
+
+# Done !
