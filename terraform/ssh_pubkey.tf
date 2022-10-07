2454628bdd11660ed0f57c0c9242041e26a0e291
Harry Kodden <harry@e-tunity.nl>
2022-10-07 12:33:14 +0200

    fix ssh pubey

--- terraform/ssh_pubkey.tf
+++ terraform/ssh_pubkey.tf
@@ -1,8 +1,15 @@
+variable "default_ssh_pub" {
+  type = string
+  default = ""
+}
+
 data "local_file" "my_pubkey" {
+  count = var.default_ssh_pub == "" ? 1 : 0
   filename = pathexpand("~/.ssh/id_rsa.pub")
+
 }
 
 resource "aws_key_pair" "my_admin" {
   key_name = var.key_name
-  public_key = data.local_file.my_pubkey.content
+  public_key = var.default_ssh_pub == "" ? data.local_file.my_pubkey[0].content : var.default_ssh_pub
 }
