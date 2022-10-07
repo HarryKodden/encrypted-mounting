be2786d044fe891df20410cad1bd542df01baae9
Harry Kodden <harry@e-tunity.nl>
2022-10-07 14:22:53 +0200

    fix ci

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,6 +1,6 @@
 // ansible.tf
 
-resource "null_resource" "ansible" {
+resource "null_resource" "store_key" {
   triggers = {
     order = aws_eip.my_eip.id
   }
@@ -11,13 +11,31 @@ resource "null_resource" "ansible" {
       then
         export $(cat .env | xargs)
       fi
+      echo "${tls_private_key.my_key.private_key_pem}" > /tmp/${var.key_name}.pem
+      chmod 600 /tmp/${var.key_name}.pem
+    EOT
+  }
+
+}
+
+resource "null_resource" "ansible" {
+  triggers = {
+    order = null_resource.store_key.id
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      if [ ! -f .env ]
+      then
+        export $(cat .env | xargs)
+      fi
       ssh-keygen -R ${var.domain}
-      echo -e "[workspace]\n${var.domain}" > /tmp/${var.domain}
+      echo "[workspace]\n${var.domain}" > /tmp/${var.domain}
       sleep 60
-      ansible-playbook -u ${var.user_name} -i /tmp/${var.domain} playbook.yml
+      ansible-playbook -u ${var.user_name} -i /tmp/${var.domain} playbook.yml --private-key /tmp/${var.key_name}.pem
       rm /tmp/${var.domain}
     EOT
-    interpreter = ["/bin/bash", "-c"]
     working_dir = "../ansible"
   }
+
 }
